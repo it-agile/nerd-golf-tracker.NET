5601270d2e3bb85f0cc6aa2139bc4197e578d563
Marko Schulz <marko.schulz@gmail.com>
2011-09-13 12:16:59 +0200

    Entferne Redundanz

--- AkzeptanzTests/BallSchlagenSteps.cs
+++ AkzeptanzTests/BallSchlagenSteps.cs
@@ -6,25 +6,19 @@ namespace AkzeptanzTests
     [Binding]
     public class BallSchlagenSteps
     {
-        [When(@"ich den Ball einmal schlage,")]
-        public void WhenIchDenBallEinmalSchlage()
-        {
-            Driver.SchlageBall();
-        }
-
         private static TrackerDriver Driver
         {
             get { return TrackerDriver.instance; }
         }
 
-        [When(@"ich den Ball noch einmal schlage,")]
-        public void WhenIchDenBallNochEinmalSchlage()
+        [When(@"ich den Ball.* einmal schlage,")]
+        public void BallSchlagen()
         {
-            WhenIchDenBallEinmalSchlage();
+            Driver.SchlageBall();
         }
 
         [Then(@"zählt der NerdGolfTracker (.*) (.*)\.")]
-        public void ThenAntwortetMirDerNerdGolfTrackerDuHastEinenSchlag2(string schlagzahl, string schlagnomen)
+        public void PruefeSchlagzahl(int schlagzahl, string schlagnomen)
         {
             Assert.That(TrackerDriver.instance.GibtAntwort(), Is.EqualTo(string.Format("Du hast {0} {1}.", schlagzahl, schlagnomen)));
         }
