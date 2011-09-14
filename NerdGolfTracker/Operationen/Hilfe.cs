bb6f6caabd5d3f9a78ddc0fb44b7fd57ed57887a
Marko Schulz <marko.schulz@gmail.com>
2011-09-14 14:40:42 +0200

    Refactoring: Iteriere beim Erstellen der Hilfe ueber die Befehle

--- NerdGolfTracker/Operationen/Hilfe.cs
+++ NerdGolfTracker/Operationen/Hilfe.cs
@@ -1,15 +1,21 @@
+using System.Collections.Generic;
 using NerdGolfTracker.Befehle;
 
 namespace NerdGolfTracker.Operationen
 {
     public class Hilfe : Operation
     {
+        private List<Befehl> _befehle = new List<Befehl> {
+            new LochwechselBefehl(),
+            new SchlagBefehl(),
+            new HilfeBefehl(),
+        };
+
         public string FuehreAus(Scorecard scorecard)
         {
             return "Ich helfe dir beim Fuehren der Scorecard. Ich reagiere auf folgende Befehle: " +
-                   HilfstextFuer(new SchlagBefehl()) + ", " +
-                   HilfstextFuer(new LochwechselBefehl()) + ", " +
-                   HilfstextFuer(new HilfeBefehl()) + ".";
+                   string.Join(", ", _befehle.ConvertAll(HilfstextFuer))
+                   + ".";
         }
 
         private string HilfstextFuer(Befehl befehl)
