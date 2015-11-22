e7ae2d44c5bb794b4f278142a56996ebc3d98db3
Sven Günther <sven.guenther@it-agile.de>
2015-11-22 14:29:47 +0100

    Refactoring zu Folgeoperation: Schlagausgabe

--- NerdGolfTracker/Operationen/Schlagausgabe.cs
+++ NerdGolfTracker/Operationen/Schlagausgabe.cs
@@ -2,9 +2,16 @@ namespace NerdGolfTracker.Operationen
 {
     public class Schlagausgabe : Operation
     {
+        private Operation _folgeOperation;
+
+        public Schlagausgabe(Operation folgeOperation)
+        {
+            _folgeOperation = folgeOperation;
+        }
+
         public string FuehreAus(Scorecard scorecard)
         {
-            return string.Format("Du hast {0} Schlag {1}", scorecard.AnzahlSchlaege, new Lochausgabe().FuehreAus(scorecard));
+            return string.Format("Du hast {0} Schlag {1}", scorecard.AnzahlSchlaege, _folgeOperation.FuehreAus(scorecard));
         }
     }
 }
--- UnitTests/Operationen/SchlagausgabeTest.cs
+++ UnitTests/Operationen/SchlagausgabeTest.cs
@@ -8,13 +8,32 @@ namespace UnitTests.Operationen
     [TestFixture]
     public class SchlagausgabeTest
     {
+        private Mock<Scorecard> _scorecardStub;
+        private Schlagausgabe _schlagausgabe;
+        private Mock<Operation> _folgeOperationMock;
+            
+        [SetUp]
+        public void Init()
+        {
+            _scorecardStub = new Mock<Scorecard>();
+            _folgeOperationMock = new Mock<Operation>();
+            _schlagausgabe = new Schlagausgabe(_folgeOperationMock.Object);
+
+        }
+
         [Test]
         public void GibtSchlagzahlenAus()
         {
-            var scorecardStub = new Mock<Scorecard>();
-            scorecardStub.Setup(scorecard => scorecard.AnzahlSchlaege).Returns(1);
-            var ausgabe = new Schlagausgabe().FuehreAus(scorecardStub.Object);
-            Assert.That(ausgabe, Is.StringContaining("1 Schlag"));
+            _scorecardStub.Setup(scorecard => scorecard.AnzahlSchlaege).Returns(1);
+            Assert.That(_schlagausgabe.FuehreAus(_scorecardStub.Object), Is.StringContaining("1 Schlag"));
         }
+
+        [Test]
+        public void FuehrtFolgeOperationAus()
+        {
+            _schlagausgabe.FuehreAus(_scorecardStub.Object);
+            _folgeOperationMock.Verify(operation => operation.FuehreAus(_scorecardStub.Object));
+        }
+
     }
 }
