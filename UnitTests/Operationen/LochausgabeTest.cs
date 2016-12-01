f132e899b02c44bb62c0e770b7eba2a9bc07b129
csd <csd@CSDINTEGRATION>
2016-12-01 13:49:41 +0100

    delete commented code

--- UnitTests/Operationen/LochausgabeTest.cs
+++ UnitTests/Operationen/LochausgabeTest.cs
@@ -16,14 +16,5 @@ namespace UnitTests.Operationen
             Assert.That(new Lochausgabe().FuehreAus(scorecardStub.Object), 
                 Contains.Substring("auf dem 2.Loch"));
         }
-
-//        [Test]
-//        public void FuehrtFolgeOperationMitAus()
-//        {
-//            var operation = new Mock<Operation>();
-//            var scoreCard = new Mock<Scorecard>();
-//            new Lochausgabe(operation.Object).FuehreAus(scoreCard.Object);
-//        }
-
     }
 }
